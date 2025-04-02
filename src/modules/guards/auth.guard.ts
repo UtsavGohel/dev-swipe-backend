a3df8c4033ac1f1688e7f81663309a8045e641fe
UtsavGohel <utsavgohel2002@gmail.com>
2025-04-02 21:29:44 +0530

    add logs to debug auth error

--- src/modules/guards/auth.guard.ts
+++ src/modules/guards/auth.guard.ts
@@ -4,7 +4,10 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err, user, info) {
+    console.log('JWT Secret:', process.env.JWT_SECRET);
     if (err || !user) {
+      console.log(`🚀 ~ JwtAuthGuard ~ handleRequest ~ user:`, user);
+      console.log(`🚀 ~ JwtAuthGuard ~ handleRequest ~ err:`, err);
       // Customize the error response when JWT is invalid
       throw new HttpException(
         'Unauthorized: Token is invalid or expired',
