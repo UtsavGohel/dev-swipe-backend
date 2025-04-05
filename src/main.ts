dcc792f23280ac023e3ffd94a71b1663758879d9
UtsavGohel <utsavgohel2002@gmail.com>
2025-04-05 12:29:17 +0530

    refactor(middleware): remove request logging middleware

--- src/main.ts
+++ src/main.ts
@@ -34,11 +34,6 @@ async function bootstrap() {
     }),
   );
 
-  app.use((req, res, next) => {
-    console.log(`Incoming Request: ${req.method} ${req.url}`);
-    next();
-  });
-
   await app.listen(3000);
 }
 bootstrap();
